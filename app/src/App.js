33cdf5ac50d21e8b54fc6d8f93ffbed52368aad7
Adrian <adrianryt3@gmail.com>
2022-03-29 17:44:25 +0200

    Basic configuration of project

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter} from "react-router-dom";
+import {Route, Routes} from "react-router";
+import React from "react";
+import Home from "./Home.tsx"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+          dasda
+        <Routes>
+          <Route path="/" element={<Home />}>
+            {/*<Route index element={<Home />} />*/}
+            {/*<Route path="teams" element={<Teams />}>*/}
+            {/*  <Route path=":teamId" element={<Team />} />*/}
+            {/*  <Route path="new" element={<NewTeamForm />} />*/}
+            {/*  <Route index element={<LeagueStandings />} />*/}
+            {/*</Route>*/}
+          </Route>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ app/src/Home.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+
+const Home = () => {
+    return (
+        <div> SIEMANO</div>
+    )
+}
+export default Home;
