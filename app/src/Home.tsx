b2b0f95538b48aacac438ddc5e9068de8278d725
Adrian <adrianryt3@gmail.com>
2022-04-15 10:49:35 +0200

    Add 'Student Join Session Form'

--- app/src/Home.tsx
+++ app/src/Home.tsx
@@ -1,12 +1,48 @@
 import * as React from "react";
 import {Outlet} from "react-router-dom";
+import {Button, Card, Form} from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
+
 
 const Home = () => {
+    const navigate = useNavigate();
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        console.log("DUPA");
+        // request na endpoint sprawdzający kod
+        // request na enpoint do bazy
+        // w responsie dostaje dane z bazy: checkpoint, adress gh, readme - opis zadania
+        // przekierowanie no /student
+        navigate("/student");
+
+    }
+
+
     return (
-        <>
-            <h2 className="ms-3 mt-4">Share your code during classes</h2>
+        <div className="content">
+            <Card className="w-50 m-auto mt-5" >
+                <Card.Header as="h5">Join Session</Card.Header>
+                <Card.Body>
+                    <Card.Title>To join session provide name and entrance code</Card.Title>
+                    <Form onSubmit={handleFormSubmit}>
+                        <Form.Group className="mb-3" controlId="formUsername">
+                            <Form.Label>Github username</Form.Label>
+                            <Form.Control placeholder="Username" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formSessionCode">
+                            <Form.Label>Session Code</Form.Label>
+                            <Form.Control placeholder="Session Code" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Join Session
+                        </Button>
+                    </Form>
+                </Card.Body>
+            </Card>
             <Outlet/>
-        </>
+        </div>
     )
 }
 export default Home;
