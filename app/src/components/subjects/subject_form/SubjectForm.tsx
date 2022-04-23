99241bd846163287607cd96666e0f10ede802182
Krzysztof Gądek <305482@student.agh.edu.pl>
2022-04-23 18:06:04 +0200

    extended mocks; subject validator added; finished subject form

--- app/src/components/subjects/subject_form/SubjectForm.tsx
+++ app/src/components/subjects/subject_form/SubjectForm.tsx
@@ -3,6 +3,7 @@ import {Alert, Badge, Card, Form, FormControl, FormGroup, FormText} from "react-
 import {useEffect, useState} from "react";
 import axios from "axios";
 import Lecturer from "../../../objects/Lecturer";
+import SubjectValidator from "../../../objects/validators/SubjectValidator";
 
 const SubjectForm = () => {
 
@@ -12,6 +13,7 @@ const SubjectForm = () => {
     useEffect(() => {
         axios.get("/mocked_lecturers.json").then((response) => {
             setLecturers(response.data);
+            setLecturer(response.data[0].id)
         }).catch((e) => {
             console.error("cannot fetch lecturers: "+ e);
         })
@@ -27,6 +29,10 @@ const SubjectForm = () => {
     const handleNameChange = (e:React.ChangeEvent<HTMLInputElement>) => setName(e.target.value);
     const handleRepoNameChange = (e:React.ChangeEvent<HTMLInputElement>) => setRepoName(e.target.value);
     const handleLecturerChange = (e:React.ChangeEvent<HTMLSelectElement>) => setLecturer(e.target.value);
+
+    const [nameError, setNameError] = useState("");
+    const [repoNameError, setRepoNameError] = useState("");
+    const [lecturerError, setLecturerError] = useState("");
 
     const handleRequestSucceed = () => {
         setShowSuccessAlert(true);
@@ -47,30 +53,41 @@ const SubjectForm = () => {
 
     const handleFormSubmit: React.FormEventHandler<HTMLFormElement> = (event:React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        const subjectValidator = new SubjectValidator();
+        if(subjectValidator.validateName(name) && subjectValidator.validateRepoName(repoName) && subjectValidator.validateLecturer(lecturer)){
+            // TODO make POST/PUT request to add Subject to database
+            handleRequestSucceed();
+        }
+        else{
+            handleRequestFailed();
+        }
+        setNameError(subjectValidator.nameError);
+        setRepoNameError(subjectValidator.repoNameError);
+        setLecturerError(subjectValidator.lecturerError);
     }
 
     return(
         <Card className="w-50 ms-5 p-3 bg-light">
             <h2>Subjects  <Badge bg="secondary">New</Badge> </h2>
-            {showSuccessAlert ? <Alert variant="success">Topic Added Successfully</Alert> : null }
-            {showFailAlert ? <Alert variant="danger">An error occurred while adding topic</Alert> : null }
+            {showSuccessAlert ? <Alert variant="success">Subject Added Successfully</Alert> : null }
+            {showFailAlert ? <Alert variant="danger">An error occurred while adding subject</Alert> : null }
             <form onSubmit={handleFormSubmit}>
                 <FormGroup>
                     <label htmlFor="NameSubjectForm" >Subject</label>
                     <FormControl id="NameSubjectForm" placeholder="Enter Name" value={name} onChange={handleNameChange} />
-                    <FormText className="text-danger me-5"></FormText>
+                    <FormText className="text-danger me-5">{nameError}</FormText>
                 </FormGroup>
                 <FormGroup>
                     <label htmlFor="RepositoryNameSubjectForm">Repository Name</label>
                     <FormControl id="RepositoryNameSubjectForm" placeholder="Enter Repository Name" value={repoName} onChange={handleRepoNameChange} />
-                    <FormText className="text-danger me-5"></FormText>
+                    <FormText className="text-danger me-5">{repoNameError}</FormText>
                 </FormGroup>
                 <FormGroup>
                     <label htmlFor="LecturerSubjectForm">Lecturer</label>
                     <Form.Select value={lecturer} onChange={handleLecturerChange}>
                         {lecturersOptions}
                     </Form.Select>
-                    <FormText className="text-danger me-5"></FormText>
+                    <FormText className="text-danger me-5">{lecturerError}</FormText>
                 </FormGroup>
                 <input className="btn btn-primary" type="submit" value="Create" />
             </form>
