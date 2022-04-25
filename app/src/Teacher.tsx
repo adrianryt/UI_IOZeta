5a86d9259aeb66e402940565d4962670fdcf11c2
youngdashu <anowak2000@gmail.com>
2022-04-25 12:42:32 +0200

    changed list of mocks into links in Teacher component

--- app/src/Teacher.tsx
+++ app/src/Teacher.tsx
@@ -1,24 +1,32 @@
 import * as React from "react";
+import {Link} from "react-router-dom";
 
 const Teacher = () => {
     return (
-        <ul>
-            <li>
-                Topics
-            </li>
-            <li>
-                Add topic
-            </li>
-            <li>
-                Subjects
-            </li>
-            <li>
-                Add subject
-            </li>
-            <li>
-                Sessions
-            </li>
-        </ul>
+        <>
+            <div className="m-3 d-flex flex-column">
+                <div className="mb-3">
+                    <Link to="/topics" className="btn btn-outline-dark m-0">Topics</Link>
+                </div>
+
+                <div className="mb-3">
+                    <Link to="/topics/new" className="btn btn-outline-dark m-0">Add new Topic</Link>
+                </div>
+
+                <div className="mb-3">
+                    <Link to="/subjects" className="btn btn-outline-dark m-0>">Subjects</Link>
+                </div>
+
+                <div className="mb-3">
+                    <Link to="/subjects/new" className="btn btn-outline-dark m-0>">Add new subjects</Link>
+                </div>
+
+                <div className="mb-3">
+                    <Link to="/sessions" className="btn btn-outline-dark m-0>">Sessions</Link>
+                </div>
+            </div>
+        </>
+
     );
 }
 
