b137124331b8b360084003ac8ba02f90e8fe9b26
youngdashu <anowak2000@gmail.com>
2022-04-25 12:40:02 +0200

    base version of Sessions component

---
+++ app/src/Sessions.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+const Sessions = () => {
+
+    return (
+        <>
+            <div className="m-lg-3">
+                <h2>Current sessions</h2>
+            </div>
+
+        </>
+    );
+
+}
+
+export default Sessions;
