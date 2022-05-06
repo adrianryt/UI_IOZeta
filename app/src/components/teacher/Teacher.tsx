35bd832158fdec3528ad64a3d4a84e8cc89c650f
Krzysztof Gądek <305482@student.agh.edu.pl>
2022-05-06 02:15:23 +0200

    centered content in the main teacher view; changed button colors; added RWD

--- app/src/components/teacher/Teacher.tsx
+++ app/src/components/teacher/Teacher.tsx
@@ -1,31 +1,32 @@
 import * as React from "react";
 import {Link} from "react-router-dom";
+import "./teacher.css"
 
 const Teacher = () => {
     return (
-        <>
-            <div className="m-3 d-flex flex-column">
-                <div className="mb-3">
-                    <Link to="/topics" className="btn btn-outline-dark m-0">Topics</Link>
+        <div className="teacher-main-menu">
+            <div className="d-flex flex-column gap-lg-3 gap-2 align-content-center justify-content-center position-absolute h-100 w-100">
+                <div className="mb-3 d-flex justify-content-center">
+                    <Link to="/topics" className="btn btn-outline-dark m-0 col-xxl-3 col-xl-4 col-lg-6 col-md-8 col-sm-9 col-10 d-block">Topics</Link>
                 </div>
 
-                <div className="mb-3">
-                    <Link to="/topics/new" className="btn btn-outline-dark m-0">Add new Topic</Link>
+                <div className="mb-3 d-flex justify-content-center">
+                    <Link to="/topics/new" className="btn btn-outline-dark m-0 col-xxl-3 col-xl-4 col-lg-6 col-md-8 col-sm-9 col-10 d-block">Add new Topic</Link>
                 </div>
 
-                <div className="mb-3">
-                    <Link to="/subjects" className="btn btn-outline-dark m-0>">Subjects</Link>
+                <div className="mb-3 d-flex justify-content-center">
+                    <Link to="/subjects" className="btn btn-outline-dark m-0 col-xxl-3 col-xl-4 col-lg-6 col-md-8 col-sm-9 col-10 d-block">Subjects</Link>
                 </div>
 
-                <div className="mb-3">
-                    <Link to="/subjects/new" className="btn btn-outline-dark m-0>">Add new subjects</Link>
+                <div className="mb-3 d-flex justify-content-center">
+                    <Link to="/subjects/new" className="btn btn-outline-dark m-0 col-xxl-3 col-xl-4 col-lg-6 col-md-8 col-sm-9 col-10 d-block">Add new subjects</Link>
                 </div>
 
-                <div className="mb-3">
-                    <Link to="/sessions" className="btn btn-outline-dark m-0>">Sessions</Link>
+                <div className="mb-3 d-flex justify-content-center">
+                    <Link to="/sessions" className="btn btn-outline-dark m-0 col-xxl-3 col-xl-4 col-lg-6 col-md-8 col-sm-9 col-10 d-block">Sessions</Link>
                 </div>
             </div>
-        </>
+        </div>
 
     );
 }
