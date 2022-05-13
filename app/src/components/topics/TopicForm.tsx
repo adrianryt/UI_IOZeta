0df24ed7d0c07d56c822107115001041cd7d5199
adrianryt <adrianryt3@gmail.com>
2022-05-13 23:57:29 +0200

    make adding Topic real thing

--- app/src/components/topics/TopicForm.tsx
+++ app/src/components/topics/TopicForm.tsx
@@ -6,6 +6,13 @@ import axios from "axios";
 import Subject from "../../objects/Subject";
 import {useSearchParams} from "react-router-dom";
 
+
+type Checkpoint = {
+    title?: string,
+    description?: string,
+    commands?: string[],
+}
+
 const TopicForm = () => {
 
     const [searchParams] = useSearchParams();
@@ -14,7 +21,7 @@ const TopicForm = () => {
         if(searchParams.get("chosen_subject") !== null){
             axios.get("/subjects/"+searchParams.get("chosen_subject")).then((response) => {
                 setSubjects([response.data])
-                setSubject(response.data.id.toString())
+                setSubject(response.data.name)
             }).catch((e) => {
                 console.error("cannot fetch subject: "+e);
             })
@@ -22,7 +29,7 @@ const TopicForm = () => {
         else{
             axios.get("/mocked_subjects.json").then((response) => {
                 setSubjects(response.data)
-                response.data.length > 0 ? setSubject(response.data[0].id.toString()) : setSubject("")
+                response.data.length > 0 ? setSubject(response.data[0].name.toString()) : setSubject("")
             }).catch((e) => {
                 console.error("cannot fetch subject: "+e);
             })
@@ -31,20 +38,38 @@ const TopicForm = () => {
     }, [])
 
     const [subjects, setSubjects] = useState<Subject[]>([]);
-    const subjectOptions = subjects.map((subject) => <option key={"key-option-topic"+subject.name} value={subject.id}>{subject.name}</option>)
+    const subjectOptions = subjects.map((subject) => <option key={"key-option-topic"+subject.name} value={subject.name}>{subject.name}</option>)
+
+    const [message, setMessage] = useState<string>("");
 
     const [title, setTitle] = useState<string>("");
-    const [description, setDescription] = useState<string>("");
+    const [githubUsername, setGithubUsername] = useState<string>("");
     const [subject, setSubject] = useState<string>("");
     const [repoName, setRepoName] = useState<string>("");
+    const [repoLink, setRepoLink] = useState<string>("");
+
+    const [checkpointsNumber, setCheckpointsNumber] = useState<undefined | number>(undefined)
+    const [checkpoints, setCheckpoints] = useState<(Checkpoint | undefined)[]>([]);
 
     const [showSuccessAlert, setShowSuccessAlert] = useState<boolean>(false);
     const [showFailAlert, setShowFailAlert] = useState<boolean>(false);
 
     const handleTitleChange = (e:React.ChangeEvent<HTMLInputElement>) => setTitle(e.target.value);
-    const handleDescriptionChange = (e:React.ChangeEvent<HTMLInputElement>) => setDescription(e.target.value);
+    const handleGithubUsernameChange= (e:React.ChangeEvent<HTMLInputElement>) => setGithubUsername(e.target.value);
     const handleSubjectChange = (e:React.ChangeEvent<HTMLSelectElement>) => setSubject(e.target.value);
     const handleRepoNameChange = (e:React.ChangeEvent<HTMLInputElement>) => setRepoName(e.target.value);
+    const handleRepoLinkChange = (e:React.ChangeEvent<HTMLInputElement>) => setRepoLink(e.target.value);
+
+    const handleCheckpointNumberChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+        let currentValue = parseInt(e.target.value, 10);
+        if (!checkpoints|| currentValue > checkpoints.length) {
+            setCheckpoints([
+                ...checkpoints,
+                ...new Array(currentValue - checkpoints.length),
+            ]);
+        }
+        setCheckpointsNumber(currentValue);
+    };
 
     const handleRequestSucceed = () => {
         setShowSuccessAlert(true);
@@ -52,7 +77,6 @@ const TopicForm = () => {
             setShowSuccessAlert(false);
         }, 3000)
         setTitle("");
-        setDescription("");
         setSubject(subjects.length > 0 ? subjects[0].id.toString() : "");
         setRepoName("");
     }
@@ -67,44 +91,77 @@ const TopicForm = () => {
     const handleFormSubmit: React.FormEventHandler<HTMLFormElement> = (event:React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         const topicValidator: TopicValidator = new TopicValidator();
-        if(topicValidator.validateTitle(title) && topicValidator.validateDescription(description) && topicValidator.validateSubject(subject) && topicValidator.validateRepoName(repoName)){
+        if(topicValidator.validateTitle(title) &&
+            topicValidator.validateSubject(subject) &&
+            topicValidator.validateRepoName(repoName) &&
+            topicValidator.validateGithubUsername(githubUsername) &&
+            topicValidator.validateRepoLink(repoLink)){
             // TODO make POST/PUT request to add Topic to database
-            handleRequestSucceed();
+
+            const data = {
+                taskName: title,
+                lecturerGitNick: githubUsername,
+                repositoryName: repoName,
+                subject,
+                repositoryLink: repoLink,
+                checkpointsContent: checkpoints,
+            }
+            axios
+                .post("http://localhost:8080/task/add", data)
+                .then((response) => {
+                    if (response.status === 200) {
+                        setMessage("Topic added");
+                        handleRequestSucceed();
+                    } else {
+                        setMessage("Error while adding topic");
+                        handleRequestFailed();
+                    }
+                })
+                .catch(() => setMessage("Error while adding topic"));
         }
         else{
             handleRequestFailed();
         }
         setSubjectError(topicValidator.subjectError);
         setTitleError(topicValidator.titleError);
-        setDescriptionError(topicValidator.descriptionError);
+        setGithubUsernameError(topicValidator.githubUsernameError)
         setRepoNameError(topicValidator.repoNameError);
+        setRepoLinkError(topicValidator.repoLinkError);
     }
 
     const [titleError, setTitleError] = useState<string>("");
-    const [descriptionError, setDescriptionError] = useState<string>("");
+    const [githubUsernameError, setGithubUsernameError] = useState<string>("");
     const [subjectError, setSubjectError] = useState<string>("");
     const [repoNameError, setRepoNameError] = useState<string>("");
+    const [repoLinkError, setRepoLinkError] = useState<string>("");
+
 
     return(
         <Card className="col-md-8 col-lg-6 col-11 ms-2 mt-2 m-lg-3 p-3 bg-light">
+            {!!message && <div>{message} </div>}
             <h2>Topics  <Badge bg="secondary">New</Badge> </h2>
             {showSuccessAlert ? <Alert variant="success">Topic Added Successfully</Alert> : null }
             {showFailAlert ? <Alert variant="danger">An error occurred while adding topic</Alert> : null }
             <form onSubmit={handleFormSubmit}>
                 <FormGroup>
-                    <label htmlFor="titleTopicForm" >Title</label>
+                    <label htmlFor="titleTopicForm">Title</label>
                     <FormControl id="titleTopicForm" placeholder="Enter title" value={title} onChange={handleTitleChange} />
                     <FormText className="text-danger me-5">{titleError}</FormText>
                 </FormGroup>
                 <FormGroup>
-                    <label htmlFor="descriptionTopicForm">Description</label>
-                    <FormControl id="descriptionTopicForm" placeholder="Enter description" value={description} onChange={handleDescriptionChange} />
-                    <FormText className="text-danger me-5">{descriptionError}</FormText>
+                    <label htmlFor="githubUsernameForm">Github Username</label>
+                    <FormControl id="githubUsernameForm" placeholder="Enter github username" value={githubUsername} onChange={handleGithubUsernameChange} />
+                    <FormText className="text-danger me-5">{githubUsernameError}</FormText>
                 </FormGroup>
                 <FormGroup>
-                    <label htmlFor="descriptionTopicForm">Repository Name</label>
-                    <FormControl id="descriptionTopicForm" placeholder="Enter repository name" value={repoName} onChange={handleRepoNameChange} />
+                    <label htmlFor="repoNameForm">Repository Name</label>
+                    <FormControl id="repoNameForm" placeholder="Enter repository name" value={repoName} onChange={handleRepoNameChange} />
                     <FormText className="text-danger me-5">{repoNameError}</FormText>
+                </FormGroup>
+                <FormGroup>
+                    <label htmlFor="repoLinkForm">Repository Link</label>
+                    <FormControl id="repoLinkForm" placeholder="Enter repository link" value={repoLink} onChange={handleRepoLinkChange} />
+                    <FormText className="text-danger me-5">{repoLinkError}</FormText>
                 </FormGroup>
                 <FormGroup>
                     <label htmlFor="subjectTopicForm">Subject</label>
@@ -113,6 +170,57 @@ const TopicForm = () => {
                     </Form.Select>
                     <FormText className="text-danger me-5">{subjectError}</FormText>
                 </FormGroup>
+                <FormGroup>
+                    <label htmlFor="checkpointsNumber">Number of checkpoints</label>
+                    <FormControl id="checkpointsNumber" placeholder="Enter number of checkpoints" value={checkpointsNumber} onChange={handleCheckpointNumberChange} />
+                    <FormText className="text-danger me-5">{repoLinkError}</FormText>
+                </FormGroup>
+                {!!checkpointsNumber &&
+                    checkpoints.slice(0, checkpointsNumber).map((el, id) => (
+                        <div key={id}>
+                            <FormGroup>
+                                {id + 1}{" "}
+                                <FormControl  id="checkpointsTitle"
+                                              className="mb-2"
+                                              placeholder="Enter checkpoint title"
+                                              value={el?.title}
+                                              onChange={(e) => {
+                                                  const arr = [...checkpoints];
+                                                  arr[id] = {
+                                                      ...arr[id],
+                                                      title: e.target.value,
+                                                  }
+                                                  setCheckpoints(arr);
+                                                }}/>
+                                <FormControl id="checkpointsDescription"
+                                             className="mb-2"
+                                             placeholder="Enter checkpoint description"
+                                             value={el?.description}
+                                             onChange={(e) => {
+                                                 const arr = [...checkpoints];
+                                                 arr[id] = {
+                                                     ...arr[id],
+                                                     description: e.target.value,
+                                                 }
+                                                 setCheckpoints(arr);
+                                             }}/>
+                                <FormControl id="checkpointsDescription"
+                                             className="mb-2"
+                                             placeholder="Enter checkpoint commands separated with ':'"
+                                             value={el?.commands?.join('&')}
+                                             onChange={(e) => {
+                                                 const arr = [...checkpoints];
+                                                 arr[id] = {
+                                                     ...arr[id],
+                                                     commands: e.target.value.split('&'),
+                                                 }
+                                                 setCheckpoints(arr);
+                                             }}/>
+                            </FormGroup>
+                        </div>
+
+                    ))}
+
                 <input className="btn btn-primary" type="submit" value="Create" />
             </form>
         </Card>
