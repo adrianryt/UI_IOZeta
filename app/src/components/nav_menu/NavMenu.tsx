71d984a43cc79ec46d03cee9641020d8fa80d1c7
MCjasiux <mcjasiux@gmail.com>
2022-05-16 08:55:38 +0200

    responsiveness on subjects and topics subpages

--- app/src/components/nav_menu/NavMenu.tsx
+++ app/src/components/nav_menu/NavMenu.tsx
@@ -7,10 +7,10 @@ const NavMenu = () => {
     return(
         <nav className="my-3">
             <div className="d-flex flex-row flex-wrap justify-content-between px-3">
-                <div>
+                <div className="col-12 col-md-1">
                         <Link className="btn btn-primary d-block" to={"/"}>Home</Link>
                 </div>
-                <div id="login-button-wrapper">
+                <div id="login-button-wrapper" className="col-12 col-md-1">
                         <Link className="btn btn-secondary d-block" to={"/login"}>Login</Link>
                 </div>
             </div>
--- app/src/components/subjects/subject_list/SubjectList.tsx
+++ app/src/components/subjects/subject_list/SubjectList.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import Subject from "../../../objects/Subject";
 import SubjectComponent from "../subject/SubjectComponent";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const SubjectList = () => {
 
@@ -13,17 +13,25 @@ const SubjectList = () => {
         axios.get("/mocked_subjects.json").then((response) => {
             setSubjects(response.data);
         }).catch((e) => {
-            console.error("cannot fetch subjects: "+e);
+            console.error("cannot fetch subjects: " + e);
         })
     }, [])
 
-    const subjectComponents = subjects.map((subject) => <SubjectComponent subject={subject} key={subject.name+""+subject.lecturer.name+""+subject.lecturer.surname} />)
+    const subjectComponents = subjects.map((subject) => <SubjectComponent subject={subject} key={subject.name + "" + subject.lecturer.name + "" + subject.lecturer.surname} />)
 
-    return(
+    return (
         <div>
-            <div className="row mx-2 my-3 m-lg-3 d-flex p-3">
-                <Link to="/subjects/new" className="btn btn-secondary col-sm-6">Add new Subject</Link>
-                <Link to="/topics" className="btn btn-secondary col-sm-6">Show all topics</Link>
+            <div className="px-3 my-3 d-flex row align-items-stretch">
+                <div className=" col-12 col-md-2">
+                    <Link to="/subjects/new" className="btn btn-secondary h-100">Add new Subject</Link>
+
+                </div>
+
+
+                <div className="col-12 col-md-2">
+                    <Link to="/topics" className="btn btn-secondary h-100">Show all topics</Link>
+
+                </div>
             </div>
             <h1 className="mx-auto text-center">Subjects</h1>
             <div id="subjectsContainer">
