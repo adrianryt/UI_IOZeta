2cf9e7c1dc2476ffb0382f2996def7ea60b18765
Krzysztof Gądek <305482@student.agh.edu.pl>
2022-05-22 23:33:45 +0200

    can add session to the database

--- app/src/components/topics/Topic.tsx
+++ app/src/components/topics/Topic.tsx
@@ -1,19 +1,60 @@
 import * as React from "react";
-import {Button, Card} from "react-bootstrap";
+import {Alert, Button, Card, FormControl, FormGroup} from "react-bootstrap";
 import TopicObject from "../../objects/TopicObject";
+import {useState} from "react";
+import axios from "axios";
+import CookieService from "../../objects/services/CookieService";
+import {useNavigate} from "react-router-dom";
 
 type propsType = {
     topic: TopicObject
 }
 
 const Topic = (props: propsType) => {
-    return(
+    const [sessionName, setSessionName] = useState<string>("");
+    const [showSessionInvalid, setShowSessionInvalid] = useState<boolean>(false);
+    const navigate = useNavigate();
 
+    const handleSetSessionName = (e: React.ChangeEvent<HTMLInputElement>) => setSessionName(e.target.value);
+    const handleCreateSessionSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if(sessionName.length > 0 && sessionName.length < 100){
+            axios( {
+                url: "http://localhost:8080/sessions/create",
+                method: "POST",
+                headers: {
+                    "Authorization": `Bearer ${CookieService.getCookie("access_token")}`
+                },
+                data: {
+                    name: sessionName,
+                    task: {
+                        id: props.topic.id
+                    }
+                }
+            }).then((response) => {
+                navigate("/sessions")
+            })
+        }
+        else{
+            setShowSessionInvalid(true);
+            setTimeout(() => {
+                setShowSessionInvalid(false);
+            }, 2000);
+        }
+    }
+
+    return(
         <Card className="my-3 mx-auto border-3 col-sm-10 col-md-8 col-lg-6 col-11">
             <Card.Header className="bg bg-info">{props.topic.title} | {props.topic.subject}</Card.Header>
+            {showSessionInvalid ? <Alert variant="danger">Session name is invalid</Alert> : null}
             <Card.Body>
                 <div className="mb-3">repository name: {props.topic.repoName}</div>
-                <Button>Create Session</Button>
+                <form onSubmit={handleCreateSessionSubmit}>
+                    <FormGroup>
+                        <FormControl value={sessionName} onChange={handleSetSessionName} className="mt-3 mb-3" placeholder="enter session name"></FormControl>
+                    </FormGroup>
+                    <input className="btn btn-primary" type="submit" value="Create Session"/>
+                </form>
             </Card.Body>
         </Card>
     )
