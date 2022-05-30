f79e1f387788321f54756a6dce00c990c1768885
youngdashu <anowak2000@gmail.com>
2022-05-30 21:55:14 +0200

    empty line removed

--- app/src/components/sessions/AssignmentProgress.tsx
+++ app/src/components/sessions/AssignmentProgress.tsx
@@ -58,7 +58,6 @@ const AssignmentProgress = () => {
                                             Link to commit
                                         </a>
                                     }
-
                                 </th>
                             )}
                         </tr>
